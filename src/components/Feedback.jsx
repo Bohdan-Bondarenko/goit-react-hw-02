7c2ae92c1f8ecabb5b0db19eb97468d46b5fd36c
Bohdan-Bondarenko <121513522+Bohdan-Bondarenko@users.noreply.github.com>
2024-04-05 12:46:17 +0200

    vite

---
+++ src/components/Feedback.jsx
@@ -0,0 +1,18 @@
+const Feedback = ({ feedbackTypes, totalFeedback }) => {
+    const positivePercentage = Math.round(
+      ((feedbackTypes.good + feedbackTypes.neutral) / totalFeedback) * 100
+    );
+    return (
+      <div className="container-options">
+        <h2>Feedback Statistics</h2>
+  
+        <p>Good: {feedbackTypes.good}</p>
+        <p>Neutral: {feedbackTypes.neutral}</p>
+        <p>Bad: {feedbackTypes.bad}</p>
+        <p>Total Feedback: {totalFeedback}</p>
+        <p>Positive Percentage: {positivePercentage}%</p>
+      </div>
+    );
+  };
+  
+  export default Feedback;
---
+++ src/components/Options.jsx
@@ -0,0 +1,14 @@
+const Options = ({ updateFeedback, totalFeedback, resetFeedback }) => {
+    return (
+      <div className="container-options">
+        <h2>Leave Your Feedback</h2>
+        <button onClick={() => updateFeedback("good")}>Good 👍</button>
+        <button onClick={() => updateFeedback("neutral")}>Neutral 😐</button>
+        <button onClick={() => updateFeedback("bad")}>Bad 👎</button>
+  
+        {totalFeedback > 0 && <button onClick={resetFeedback}>Reset</button>}
+      </div>
+    );
+  };
+  
+  export default Options;
